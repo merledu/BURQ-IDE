e783603d81b887e2afadf982411259efd5775ebb
monisj <koolmonis@gmail.com>
2020-04-27 14:10:23 +0500

    Added transparent and lightGray colour to the linenumber

--- newui/LightW.py
+++ newui/LightW.py
@@ -605,7 +605,7 @@ class Ui_MainWindow(object):
         self.tabWidget_2.tabCloseRequested.connect(self.removeTab)
         self.plainTextEdit = LineNumber.QCodeEditor(a)
         self.highlighter=hightest.Highlighter(self.plainTextEdit.document())
-        self.plainTextEdit.setGeometry(QtCore.QRect(0, 0, 841, 491))
+        #self.plainTextEdit.setGeometry(QtCore.QRect(0, 0, 841, 491))
         self.plainTextEdit.setStyleSheet("background-color: rgb(255, 255, 255,88%);\n"
 "")
         self.plainTextEdit.setObjectName("plainTextEdit")
@@ -635,7 +635,7 @@ class Ui_MainWindow(object):
         self.tabWidget_2.tabCloseRequested.connect(self.removeTab)
         self.plainTextEdit = LineNumber.QCodeEditor(a)
         self.highlighter=hightest.Highlighter(self.plainTextEdit.document())
-        self.plainTextEdit.setGeometry(QtCore.QRect(0, 0, 841, 491))
+        #self.plainTextEdit.setGeometry(QtCore.QRect(0, 0, 841, 491))
         self.plainTextEdit.setStyleSheet("background-color: rgb(255, 255, 255,88%);\n"
 "")
         self.plainTextEdit.setObjectName("plainTextEdit")
