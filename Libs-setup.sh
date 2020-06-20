c2cc4d392767abfd1f0f7972c90b4ef9f2654904
monisj <koolmonis@gmail.com>
2020-06-20 11:59:19 +0500

    Run the libs-setup.sh for libraries and Gnu-toolchain-setup.sh for the compiler

--- Libs-setup.sh
+++ Libs-setup.sh
@@ -39,53 +39,12 @@ sudo apt-get -y install gcc-multilib libc6-i386 #This may or maynot work as the 
 sudo apt install Zlib #This may or maynot work as the mirror seems missing
 sudo apt-get install tree
 sudo apt-get install sip
+pip3 install sip
 sleep 1
-
-sudo apt-get update && apt-get install \
-    -y --no-install-recommends \
-    build-essential \
-    git \
-    ca-certificates \
-    wget \
-    cmake \
-    pkg-config \
-    python3-dev \
-    libglib2.0-dev \
-    qt5-default \
-    qttools5-dev
-
-mkdir -p /tmp/EAF
-
-cd /tmp/EAF && \
-    git clone https://github.com/lxqt/lxqt-build-tools.git \
-    && cd lxqt-build-tools \
-    && mkdir build && cd build \
-    && cmake .. \
-    && make && sudo make install
-
-cd /tmp/EAF && \
-    wget https://www.riverbankcomputing.com/static/Downloads/sip/4.19.19/sip-4.19.19.tar.gz && \
-    tar xvzf sip-4.19.19.tar.gz && \
-    cd sip-4.19.19 && \
-    python3 configure.py --sip-module PyQt5.sip && \
-    make && \
-    sudo make install
-
-cd /tmp/EAF && \
-    wget https://www.riverbankcomputing.com/static/Downloads/PyQt5/5.13.2/PyQt5-5.13.2.tar.gz && \
-    tar xvzf PyQt5-5.13.2.tar.gz && \
-    cd PyQt5-5.13.2 && \
-    python3 configure.py --confirm-license && \
-    make && \
-    sudo make install
-
-cd /tmp/EAF && \
-    git clone https://github.com/lxqt/qtermwidget \
-    && cd qtermwidget \
-    && mkdir build && cd build \
-    && cmake .. -DQTERMWIDGET_BUILD_PYTHON_BINDING=ON -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_INSTALL_LIBDIR=/lib \
-    && make && sudo make install
-logout
+sudo -H pip3 install -U pyqt5 pyqtwebengine
+sudo apt install python3-sip-dev python3-pyqt5
+sudo ./qtermwidget-setup.sh
+sleep 1
 cd /tmp/EAF && cd qtermwidget      
 && cd build     
 && cmake .. -DQTERMWIDGET_BUILD_PYTHON_BINDING=ON -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_INSTALL_LIBDIR=/lib     
